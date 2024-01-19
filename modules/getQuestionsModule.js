60e9f9a1fd09a7491e9f376e382ca6a6086be28b
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-01-18 23:46:39 -0300

    Add files via upload

---
+++ modules/getQuestionsModule.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+const validateApiKey = require("./validateApiKey");
+
+router.get("/", validateApiKey, async (req, res) => {
+  try {
+    const users = await db.any("SELECT * FROM perguntas");
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Erro ao obter usuários:", error);
+    res.status(500).json({ error: "Erro ao obter usuários" });
+  }
+});
+
+module.exports = router;
