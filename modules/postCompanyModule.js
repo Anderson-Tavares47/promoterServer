184cedb21226a0daf790d9f30f6cb88b83060725
emanoelsaraivas@gmail.com <emanoelsaraivas@gmail.com>
2024-01-26 14:05:06 -0300

    updated put/post

--- modules/postCompanyModule.js
+++ modules/postCompanyModule.js
@@ -1,28 +1,21 @@
-const express = require("express");
-const router = express.Router();
-const db = require("../db");
-const validateApiKey = require("./validateApiKey");
-
 router.post("/", validateApiKey, async (req, res) => {
-  try {
-    const {
-      razaoSocial,
-      nomeFantasia,
-      logradouro,
-      numero,
-      cidade,
-      estado,
-      nomeResponsavel,
-      email,
-      telefone,
-      bairro,
-      cnpj,
-    } = req.body;
+  const {
+    razaoSocial,
+    nomeFantasia,
+    logradouro,
+    numero,
+    cidade,
+    estado,
+    nomeResponsavel,
+    email,
+    telefone,
+    bairro,
+    cnpj, // Adicionado o campo CNPJ
+  } = req.body;
 
-    const newEmpresa = await db.one(
-      `INSERT INTO empresa (razaoSocial, nomeFantasia, logradouro, numero, cidade, estado, nomeResponsavel, email, telefone, bairro, cnpj) 
-       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) 
-       RETURNING *`,
+  try {
+    const result = await db.one(
+      "INSERT INTO empresa (razao_social, nome_fantasia, logradouro, numero, cidade, estado, nome_responsavel, email, telefone, bairro, cnpj) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *",
       [
         razaoSocial,
         nomeFantasia,
@@ -38,11 +31,9 @@ router.post("/", validateApiKey, async (req, res) => {
       ]
     );
 
-    res.status(201).json(newEmpresa);
+    res.status(201).json(result);
   } catch (error) {
-    console.error("Erro ao criar empresa:", error);
-    res.status(500).json({ error: "Erro ao criar empresa" });
+    console.error("Erro ao cadastrar empresa:", error);
+    res.status(500).json({ error: "Erro ao cadastrar empresa" });
   }
 });
-
-module.exports = router;
--- modules/putCompanyModule.js
+++ modules/putCompanyModule.js
@@ -1,30 +1,22 @@
-const express = require("express");
-const router = express.Router();
-const db = require("../db");
-const validateApiKey = require("./validateApiKey");
-
 router.put("/:id", validateApiKey, async (req, res) => {
-  try {
-    const {
-      razaoSocial,
-      nomeFantasia,
-      logradouro,
-      numero,
-      cidade,
-      estado,
-      nomeResponsavel,
-      email,
-      telefone,
-      bairro,
-      cnpj,
-    } = req.body;
+  const empresaId = req.params.id;
+  const {
+    razaoSocial,
+    nomeFantasia,
+    logradouro,
+    numero,
+    cidade,
+    estado,
+    nomeResponsavel,
+    email,
+    telefone,
+    bairro,
+    cnpj, // Adicionado o campo CNPJ
+  } = req.body;
 
-    const updatedEmpresa = await db.one(
-      `UPDATE empresa 
-       SET razaoSocial = $1, nomeFantasia = $2, logradouro = $3, numero = $4, cidade = $5, estado = $6, 
-           nomeResponsavel = $7, email = $8, telefone = $9, bairro = $10,
-       WHERE id = $12
-       RETURNING *`,
+  try {
+    const result = await db.one(
+      "UPDATE empresa SET razao_social = $1, nome_fantasia = $2, logradouro = $3, numero = $4, cidade = $5, estado = $6, nome_responsavel = $7, email = $8, telefone = $9, bairro = $10, cnpj = $11 WHERE id = $12 RETURNING *",
       [
         razaoSocial,
         nomeFantasia,
@@ -37,15 +29,13 @@ router.put("/:id", validateApiKey, async (req, res) => {
         telefone,
         bairro,
         cnpj,
-        req.params.id,
+        empresaId,
       ]
     );
 
-    res.status(200).json(updatedEmpresa);
+    res.status(200).json(result);
   } catch (error) {
     console.error("Erro ao atualizar empresa:", error);
     res.status(500).json({ error: "Erro ao atualizar empresa" });
   }
 });
-
-module.exports = router;
