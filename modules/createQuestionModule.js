2713d759139cc79e3275c3243e13ae467a9d07ed
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-01-29 02:19:15 -0300

    Update createQuestionModule.js

--- modules/createQuestionModule.js
+++ modules/createQuestionModule.js
@@ -7,26 +7,23 @@ router.post("/:idPesquisa", validateApiKey, async (req, res) => {
   const idPesquisa = req.params.idPesquisa;
   const perguntas = req.body;
   console.log(idPesquisa);
-  console.log(perguntas)
+  console.log(perguntas);
 
   try {
-  await db.tx(async (t) => {
-    await t.none("BEGIN");
-    await Promise.all(perguntas.map(async (pergunta) => {
-      await t.none(
-        "INSERT INTO perguntas (id_pesquisa, pergunta, tipo_pergunta, respostas, obrigatoriedade) VALUES ($1, $2, $3, $4, $5)",
-        [idPesquisa, pergunta.pergunta, pergunta.tipo_pergunta, pergunta.respostas, pergunta.obrigatoriedade]
-      );
-    }));
-    await t.none("COMMIT");
-  });
+    if (perguntas && perguntas.length > 0) {
+      await Promise.all(perguntas.map(async (pergunta) => {
+        await db.none(
+          "INSERT INTO perguntas (id_pesquisa, pergunta, tipo_pergunta, respostas, obrigatoriedade) VALUES ($1, $2, $3, $4, $5)",
+          [idPesquisa, pergunta.pergunta, pergunta.tipo_pergunta, pergunta.respostas, pergunta.obrigatoriedade]
+        );
+      }));
+    }
 
-  res.status(201).json({ message: "Perguntas associadas à pesquisa criadas com sucesso" });
-} catch (error) {
-  console.error("Erro ao criar perguntas associadas à pesquisa:", error);
-  res.status(500).json({ error: "Erro ao criar perguntas associadas à pesquisa" });
-}
-
+    res.status(201).json({ message: "Perguntas associadas à pesquisa criadas com sucesso" });
+  } catch (error) {
+    console.error("Erro ao criar perguntas associadas à pesquisa:", error);
+    res.status(500).json({ error: "Erro ao criar perguntas associadas à pesquisa" });
+  }
+});
 
 module.exports = router;
-
