854365f2431c66da52efec487b651d20775de4f6
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-01-29 01:59:03 -0300

    Update createQuestionModule.js

--- modules/createQuestionModule.js
+++ modules/createQuestionModule.js
@@ -1,19 +1,12 @@
-const express = require("express");
-const router = express.Router();
-const db = require("../db");
-const validateApiKey = require("./validateApiKey");
-
 router.post("/:idPesquisa", validateApiKey, async (req, res) => {
   const idPesquisa = req.params.idPesquisa;
   const perguntas = req.body;
-  console.log(idPesquisa);
-  console.log(perguntas)
-
+  
   try {
     if (perguntas && perguntas.length > 0) {
       await Promise.all(perguntas.map(async (pergunta) => {
-        await db.none(
-          "INSERT INTO perguntas (id_pesquisa, pergunta, tipo_pergunta, respostas, obrigatoriedade) VALUES ($1, $2, $3, $4, $5)",
+        await db.one(
+          "INSERT INTO perguntas (id_pesquisa, pergunta, tipo_pergunta, respostas, obrigatoriedade) VALUES ($1, $2, $3, $4, $5) RETURNING *",
           [idPesquisa, pergunta.pergunta, pergunta.tipo_pergunta, pergunta.respostas, pergunta.obrigatoriedade]
         );
       }));
@@ -26,4 +19,3 @@ router.post("/:idPesquisa", validateApiKey, async (req, res) => {
   }
 });
 
-module.exports = router;
