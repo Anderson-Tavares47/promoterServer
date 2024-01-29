c35788bc0069f84c156517aa6602f638fdc0499b
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-01-29 02:43:13 -0300

    Update createQuestionModule.js

--- modules/createQuestionModule.js
+++ modules/createQuestionModule.js
@@ -10,6 +10,7 @@ router.post("/:idPesquisa", validateApiKey, async (req, res) => {
   try {
     if (perguntas && perguntas.length > 0) {
       await Promise.all(perguntas.map(async (pergunta) => {
+        console.log(pergunta)
         await db.oneOrNone(
           "INSERT INTO perguntas (id_pesquisa, pergunta, tipo_pergunta, respostas, obrigatoriedade) VALUES ($1, $2, $3, $4, $5)",
           [
