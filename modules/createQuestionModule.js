fba3ce6509a2807c436432dd6aff20116213d682
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-01-29 03:06:23 -0300

    Update createQuestionModule.js

--- modules/createQuestionModule.js
+++ modules/createQuestionModule.js
@@ -5,31 +5,23 @@ const validateApiKey = require("./validateApiKey");
 
 router.post("/:idPesquisa", validateApiKey, async (req, res) => {
   const idPesquisa = req.params.idPesquisa;
-  const perguntas = req.body;
-  console.log(perguntas)
-  
-  try {
-    if (perguntas && perguntas.length > 0) {
-      await Promise.all(perguntas.map(async (pergunta) => {
-        await db.oneOrNone(
-          "INSERT INTO perguntas (id_pesquisa, pergunta, tipo_pergunta, respostas, obrigatoriedade) VALUES ($1, $2, $3, $4, $5)",
-          [
-            String(idPesquisa),
-            String(pergunta.pergunta),
-            String(pergunta.tipo_pergunta),
-            String(pergunta.respostas),
-            String(pergunta.obrigatoriedade)
-          ]
-        );
-      }));
-    }
+  const {
+    id_pesquisa, pergunta, tipo_pergunta, respostas, obrigatoriedade
+  } = req.body;
 
-    res.status(201).json({ message: "Perguntas associadas à pesquisa criadas com sucesso" });
+  try {
+    const result = await db.one(
+      "INSERT INTO perguntas (id_pesquisa, pergunta, tipo_pergunta, respostas, obrigatoriedade) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [
+        idPesquisa, pergunta, tipo_pergunta, respostas, obrigatoriedade
+      ]
+    );
+
+    res.status(201).json(result);
   } catch (error) {
-    console.error("Erro ao criar perguntas associadas à pesquisa:", error);
-    res.status(500).json({ error: "Erro ao criar perguntas associadas à pesquisa" });
+    console.error("Erro ao cadastrar empresa:", error);
+    res.status(500).json({ error: "Erro ao cadastrar empresa" });
   }
 });
 
 module.exports = router;
-
