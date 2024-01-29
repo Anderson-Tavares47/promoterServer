2c751f8b9294698e5e304ac70baca05d31badef6
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-01-29 01:31:45 -0300

    Update createSearchModule.js

--- modules/createSearchModule.js
+++ modules/createSearchModule.js
@@ -4,28 +4,15 @@ const db = require("../db");
 const validateApiKey = require("./validateApiKey");
 
 router.post("/", validateApiKey, async (req, res) => {
-  const { nome_pesquisa, colaboradores_responsaveis, obrigatoriedade, status, perguntas } = req.body;
+  const { nome_pesquisa, colaboradores_responsaveis, obrigatoriedade, status } = req.body;
 
   try {
-    // Insira a pesquisa na tabela de pesquisas
     const result = await db.one(
       "INSERT INTO pesquisas (nome_pesquisa, colaboradores_responsaveis, obrigatoriedade, status) VALUES ($1, $2, $3, $4) RETURNING id",
       [nome_pesquisa, colaboradores_responsaveis, obrigatoriedade, status]
     );
 
-    const idPesquisa = result.id;
-
-    // Insira as perguntas na tabela de perguntas associadas à pesquisa
-    if (perguntas && perguntas.length > 0) {
-      await Promise.all(perguntas.map(async (pergunta) => {
-        await db.none(
-          "INSERT INTO perguntas (id_pesquisa, pergunta, tipo_pergunta, respostas, obrigatoriedade) VALUES ($1, $2, $3, $4, $5)",
-          [idPesquisa, pergunta.pergunta, pergunta.tipo_pergunta, pergunta.respostas, pergunta.obrigatoriedade]
-        );
-      }));
-    }
-
-    res.status(201).json({ message: "Pesquisa criada com sucesso" });
+    res.status(201).json({ message: "Pesquisa criada com sucesso", idPesquisa: result.id });
   } catch (error) {
     console.error("Erro ao criar pesquisa:", error);
     res.status(500).json({ error: "Erro ao criar pesquisa" });
@@ -33,3 +20,4 @@ router.post("/", validateApiKey, async (req, res) => {
 });
 
 module.exports = router;
+
