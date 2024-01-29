e3ddbea85b4227763b950bf5f6c633204c253a9d
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-01-29 01:00:43 -0300

    Update createSearchModule.js

--- modules/createSearchModule.js
+++ modules/createSearchModule.js
@@ -4,30 +4,25 @@ const db = require("../db");
 const validateApiKey = require("./validateApiKey");
 
 router.post("/", validateApiKey, async (req, res) => {
+  const { nome_pesquisa, colaboradores_responsaveis, obrigatoriedade, status, perguntas } = req.body;
+
   try {
-    const { nome_pesquisa, colaboradores_responsaveis, obrigatoriedade, status, perguntas } = req.body;
+    // Insira a pesquisa na tabela de pesquisas
+    const result = await db.one(
+      "INSERT INTO pesquisas (nome_pesquisa, colaboradores_responsaveis, obrigatoriedade, status) VALUES ($1, $2, $3, $4) RETURNING id",
+      [nome_pesquisa, colaboradores_responsaveis, obrigatoriedade, status]
+    );
 
-    // Se os campos obrigatórios não estiverem presentes, apenas ignore a inserção na tabela de pesquisas
-    if (!nome_pesquisa || colaboradores_responsaveis || !obrigatoriedade || !status) {
-      console.log("Campos obrigatórios ausentes. Continuando sem inserir na tabela de pesquisas.");
-    } else {
-      // Insira a pesquisa na tabela de pesquisas apenas se os campos obrigatórios estiverem presentes
-      const result = await db.one(
-        "INSERT INTO pesquisas (nome_pesquisa, colaboradores_responsaveis, obrigatoriedade, status) VALUES ($1, $2, $3, $4) RETURNING id",
-        [nome_pesquisa, colaboradores_responsaveis, obrigatoriedade, status]
-      );
+    const idPesquisa = result.id;
 
-      const idPesquisa = result.id;
-
-      // Insira as perguntas na tabela de perguntas associadas à pesquisa
-      if (perguntas && perguntas.length > 0) {
-        await Promise.all(perguntas.map(async (pergunta) => {
-          await db.none(
-            "INSERT INTO perguntas (id_pesquisa, pergunta, tipo_pergunta, respostas, obrigatoriedade) VALUES ($1, $2, $3, $4, $5)",
-            [idPesquisa, pergunta.pergunta, pergunta.tipo_pergunta, pergunta.respostas, pergunta.obrigatoriedade]
-          );
-        }));
-      }
+    // Insira as perguntas na tabela de perguntas associadas à pesquisa
+    if (perguntas && perguntas.length > 0) {
+      await Promise.all(perguntas.map(async (pergunta) => {
+        await db.none(
+          "INSERT INTO perguntas (id_pesquisa, pergunta, tipo_pergunta, respostas, obrigatoriedade) VALUES ($1, $2, $3, $4, $5)",
+          [idPesquisa, pergunta.pergunta, pergunta.tipo_pergunta, pergunta.respostas, pergunta.obrigatoriedade]
+        );
+      }));
     }
 
     res.status(201).json({ message: "Pesquisa criada com sucesso" });
@@ -38,4 +33,3 @@ router.post("/", validateApiKey, async (req, res) => {
 });
 
 module.exports = router;
-
